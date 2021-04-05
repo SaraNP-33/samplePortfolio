e7f8628f27b077903b3e24f35c945fc9096981a6
Sara Neves Pereira <sara.neves.pereira@gmail.com>
2021-04-05 12:39:20 +0000

    sidenav working, in need of more css

--- sample-portfolio/src/components/Nav/index.js
+++ sample-portfolio/src/components/Nav/index.js
@@ -1,19 +1,22 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { slide as Menu} from 'react-burger-menu';
+
+
+
 function Nav(){
     return(
     <Menu>
         <ul className='nav-items'>
-            <li className='ni -1'>
+            <li className='ni-1'>
                 <a href='/'>Home</a>
             </li>
-            <li className='ni -1'>
+            <li className='ni-1'>
                 <a href='/about'>About</a>
             </li>
-            <li className='ni -1'>
+            <li className='ni-1'>
                 <a href='/gallery'>Gallery</a>
             </li>
-            <li className='ni -1'>
+            <li className='ni-1'>
                 <a href='/contact'>Contact</a>
             </li>
             
